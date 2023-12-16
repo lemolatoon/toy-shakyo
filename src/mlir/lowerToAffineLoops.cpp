88c887c4390508dc220f3d8178c3b756ea29cd91
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-12-16 19:56:03 +0900

    Impl FuncOpLowering, PrintOpLowering, ReturnOpLowering, TransposeOpLowering, ConstantOpLowering

--- src/mlir/lowerToAffineLoops.cpp
+++ src/mlir/lowerToAffineLoops.cpp
@@ -1,7 +1,9 @@
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinDialect.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Diagnostics.h"
 #include "mlir/IR/DialectRegistry.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/PatternMatch.h"
@@ -16,6 +18,7 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include <algorithm>
 #include <memory>
 
 // ToyToAffine RewritePatterns
@@ -137,6 +140,175 @@ using AddOpLowering = BinaryOpLowering<toy::AddOp, mlir::arith::AddFOp>;
 using MulOpLowering = BinaryOpLowering<toy::MulOp, mlir::arith::MulFOp>;
 
 // ToyToAffine RewritePatterns : Constant operations
+struct ConstantOpLowering : public mlir::OpRewritePattern<toy::ConstantOp> {
+  using OpRewritePattern<toy::ConstantOp>::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(toy::ConstantOp op,
+                  mlir::PatternRewriter &rewriter) const final {
+    mlir::DenseElementsAttr constantValue = op.getValue();
+    mlir::Location loc = op.getLoc();
+
+    // When lowering the constant operation, we allocate and assign the constant
+    // values to a corresponding memref allocation.
+    auto tensorType = llvm::cast<mlir::RankedTensorType>(op.getType());
+    auto memRefType = convertTensorToMemRef(tensorType);
+    auto alloc = insertAllocAndDealloc(memRefType, loc, rewriter);
+
+    // We will be generating constant indices up-to the largest dimension.
+    // Create these constants up-front to avoid large amounts of redundant
+    // operations.
+    auto valueShape = memRefType.getShape();
+    llvm::SmallVector<mlir::Value, 8> constantIndices;
+
+    // 使う可能性のあるindexをここで生成しておく。
+    if (!valueShape.empty()) {
+      for (auto i : llvm::seq<int64_t>(
+               0, *std::max_element(valueShape.begin(), valueShape.end()))) {
+        constantIndices.push_back(
+            rewriter.create<mlir::arith::ConstantIndexOp>(loc, i));
+      }
+    } else {
+      // This is the case of a tensor of rank 0.
+      constantIndices.push_back(
+          rewriter.create<mlir::arith::ConstantIndexOp>(loc, 0));
+    }
+
+    // The constant operation represents a multi-dimensional constant, so we
+    // will need to generate a store for each of the elements. The following
+    // functor recursively walks the dimensions of the constant shape,
+    // generating a store when the recursion hits the base case.
+    llvm::SmallVector<mlir::Value, 2> indices;
+    auto valueIt = constantValue.value_begin<mlir::FloatAttr>();
+    std::function<void(int64_t)> storeElements = [&](uint64_t dimension) {
+      // The last dimension is the base case of the recursion, at this point
+      // we store the element at the given index.
+      if (dimension == valueShape.size()) {
+        rewriter.create<mlir::AffineStoreOp>(
+            loc, rewriter.create<mlir::arith::ConstantOp>(loc, *valueIt++),
+            alloc, llvm::ArrayRef(indices));
+      }
+
+      // Otherwise, iterate over the current dimension and add the indices to
+      // the list.
+      for (uint64_t i = 0, e = valueShape[dimension]; i < e; i++) {
+        indices.push_back(constantIndices[i]);
+        storeElements(dimension + 1);
+        indices.pop_back();
+      }
+    };
+
+    // ```
+    // var a = [[1, 2, 3], [4, 5, 6]]
+    // ```
+    // のような配列の場合、
+    // [] -> [0] -> [0, 1] -> [0] -> [0, 2] -> ...
+    // のようにindicesが変動して、storeElementsが呼ばれる。
+    // Start the element storing recursion from the first dimension.
+    storeElements(/*dimension=*/0);
+
+    // Replace this operation with the generated alloc.
+    rewriter.replaceOp(op, alloc);
+    return mlir::success();
+  }
+};
+
+// ToyToAffine RewritePatterns : Func operations
+
+struct FuncOpLowering : public mlir::OpConversionPattern<toy::FuncOp> {
+  using mlir::OpConversionPattern<toy::FuncOp>::OpConversionPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(toy::FuncOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const final {
+    // We only lower the main function as we expect that all other functions
+    // have been inlined.
+    if (op.getName() != "main") {
+      return mlir::failure();
+    }
+
+    // Verify that the given main has no inputs and results.
+    if (op.getNumArguments() || op.getFunctionType().getNumResults()) {
+      return rewriter.notifyMatchFailure(op, [](mlir::Diagnostic &diag) {
+        diag << "expected 'main' to have 0 inputs and 0 results";
+      });
+    }
+
+    // Create a new non-toy function, with the same region.
+    auto func = rewriter.create<mlir::func::FuncOp>(op.getLoc(), op.getName(),
+                                                    op.getFunctionType());
+    rewriter.inlineRegionBefore(op.getRegion(), func.getBody(), func.end());
+    rewriter.eraseOp(op);
+
+    return mlir::success();
+  }
+};
+
+// ToyToAffine RewritePatterns : Print operations
+
+struct PrintOpLowering : public mlir::OpConversionPattern<toy::PrintOp> {
+  using OpConversionPattern<toy::PrintOp>::OpConversionPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(toy::PrintOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const final {
+    // We don't lower "toy.print" in this pass, but we need to update its
+    // operands.
+    rewriter.updateRootInPlace(op,
+                               [&] { op->setOperands(adaptor.getOperands()); });
+
+    return mlir::success();
+  }
+};
+
+// ToyToAffine RewritePatterns : Return operations
+struct ReturnOpLowering : public mlir::OpRewritePattern<toy::ReturnOp> {
+  using mlir::OpRewritePattern<toy::ReturnOp>::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(toy::ReturnOp op,
+                  mlir::PatternRewriter &rewriter) const final {
+    // During this lowering, we expect that all functions have been inlined.
+    if (op.hasOperand()) {
+      return mlir::failure();
+    }
+    // We lower "toy.return" directly to "func.return"
+    rewriter.replaceOpWithNewOp<mlir::func::ReturnOp>(op);
+    return mlir::success();
+  }
+};
+
+// ToyToAffine RewritePatterns : Transpose operations
+struct TransposeOpLowering : public mlir::ConversionPattern {
+  TransposeOpLowering(mlir::MLIRContext *ctx)
+      : mlir::ConversionPattern(toy::TransposeOp::getOperationName(), 1, ctx) {}
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::Operation *op, mlir::ArrayRef<mlir::Value> operands,
+                  mlir::ConversionPatternRewriter &rewriter) const final {
+    auto loc = op->getLoc();
+
+    lowerOpToLoops(
+        op, operands, rewriter,
+        [loc](mlir::OpBuilder &builder, mlir::ValueRange memRefOperands,
+              mlir::ValueRange loopIvs) {
+          // https://mlir.llvm.org/docs/DefiningDialects/Operations/#operand-adaptors
+          // Generate an adaptor for the remapped operands of the
+          // TransposeOp. This allows for using the nice named
+          // accessors that are generated by the ODS.
+          toy::TransposeOpAdaptor transposeAdaptor(memRefOperands);
+          mlir::Value input = transposeAdaptor.getInput();
+
+          // Transpose the elements by generating a load from the reverse
+          // indices.
+          llvm::SmallVector<mlir::Value, 2> reverseIvs(llvm::reverse(loopIvs));
+          // returnしたmlir::Valueは、loopIvsでstoreされる。
+          return builder.create<mlir::AffineLoadOp>(loc, input, reverseIvs);
+        });
+
+    return mlir::success();
+  }
+};
 
 } // namespace
 
@@ -187,7 +359,9 @@ void ToyToAffineLoweringPass::runOnOperation() {
   // the set of patterns that will lower the Toy operations.
   mlir::RewritePatternSet patterns(&getContext());
   // TODO: Add patterns here
-  patterns.add<AddOpLowering, MulOpLowering>(&getContext());
+  patterns.add<AddOpLowering, MulOpLowering, FuncOpLowering, PrintOpLowering,
+               ReturnOpLowering, TransposeOpLowering, ConstantOpLowering>(
+      &getContext());
 
   // With the target and rewrite patterns defined, we can now attempt the
   // conversion. The conversion will signal failure if any of our `illegal`
